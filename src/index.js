52fa90e102b0508b38d5e259034ac0578046be5a
Michael Martinez <zeroeclipse@live.com>
2021-07-29 14:46:19 -0700

    commit tree 46762386b4e1b8b1317bdb9117fc23fbc838d8bc

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 // write your createStore function here
+function createStore() {
+  let state;
+  // state is now accessible to dispatch
+  function getState() {
+    return state
+  }
+  function dispatch(action) {
+    state = candyReducer(state, action);
+    // render();
+  }
+  return {dispatch, getState}
+}
 
 function candyReducer(state = [], action) {
   switch (action.type) {
@@ -17,6 +29,8 @@ function render() {
     throw new Error("the store's state has not been defined yet")
   }
 };
+
+store = createStore(candyReducer)
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
